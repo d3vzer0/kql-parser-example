bdd0aacc1c774b0837799069aaa675ab6b4f3a8f
Joey Dreijer <jdreijer@d3vzer0.com>
2023-05-19 00:52:31 +0200

    Update main.py

--- kustochecker/main.py
+++ kustochecker/main.py
@@ -11,19 +11,22 @@ Reflection.Assembly.LoadFile(file_path)
 # Import the KustoCode function from Kusto.Language class (loaded via DLL)
 from Kusto.Language import KustoCode
 
-# Parse the incorrect KQL query
-code = KustoCode.Parse('randomTable| bla')
+# Parse the incorrect example KQL query
+example_query = 'randomTable| bla'
+code = KustoCode.Parse(example_query)
 
 # Run diagnostics, print issues when there are errors found (ie. count > 0)
 diagnostics = code.GetDiagnostics()
 if (diagnostics.Count > 0):
     for diag in diagnostics:
         message = {
+            'query': example_query,
             'severity': diag.Severity,
             'message': diag.Message,
             'length': diag.Length,
             'start': diag.Start,
-            'end': diag.End
+            'end': diag.End,
+            'troublemaker': example_query[diag.Start:diag.End]
         }
         print(message)
 
