bb65a2a1f2fffeaca700604820787b2d8e1dbb94
Joey Dreijer <jdreijer@d3vzer0.com>
2023-05-19 15:55:02 +0200

    Added ParseAndAnalyze example

--- kustochecker/main.py
+++ kustochecker/main.py
@@ -2,21 +2,37 @@ import clr
 import os
 from System import Reflection
 
+
 # Get current path and append location of the DLL 
 file_path = f'{os.getcwd()}/Kusto.Language.dll'
 
 # Load the DLL (requires absolute path)
 Reflection.Assembly.LoadFile(file_path)
 
-# Import the KustoCode function from Kusto.Language class (loaded via DLL)
+# Import the KustoCode method from Kusto.Language class (loaded via DLL)
 from Kusto.Language import KustoCode
+from Kusto.Language.Symbols import DatabaseSymbol, TableSymbol 
+from Kusto.Language import GlobalState
 
-# Parse the incorrect example KQL query
-example_query = 'randomTable| bla'
-code = KustoCode.Parse(example_query)
+
+# Parse the incorrect example KQL query (sorty instead of sort)
+example_query = '''SigninLogs
+    | where TimeGenerated >= ago(7d)
+    | sorty by TimeGenerated, Identity desc
+    | take 5'''
+
+## Simple example only performing base KQL syntax checking
+code = KustoCode.parse(example_query)
+
+## ParseAndAnalyze example with GlobalState
+# Add an incorrect global state (str instead of datetime for TimeGenerated) 
+#global_state = GlobalState.Default.WithDatabase(DatabaseSymbol('db', 
+#                TableSymbol('SigninLogs', '(TimeGenerated: str, Identity: str')))
+#code = KustoCode.ParseAndAnalyze(example_query, global_state)
 
 # Run diagnostics, print issues when there are errors found (ie. count > 0)
 diagnostics = code.GetDiagnostics()
+
 if (diagnostics.Count > 0):
     for diag in diagnostics:
         message = {
